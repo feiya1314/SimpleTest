a7fc78a5000be9eb36d2f4bcb321b596b05c5150
feiya1314 <yufei1313@gmail.com>
2019-08-13 00:25:11 +0800

    添加interrupt test

---
+++ src/main/java/mytest/interrupt/BlockTask.java
@@ -0,0 +1,27 @@
+package mytest.interrupt;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class BlockTask {
+    private BlockResource resource = new BlockResource();
+    public String getResource(int code) throws InterruptedException{
+        if (Thread.interrupted())
+            throw new InterruptedException();
+        System.out.println("start get resource");
+
+        return doGetResource(code);
+    }
+
+    private String doGetResource(int code) throws InterruptedException{
+        new Thread(new BlockThread(Thread.currentThread(),resource,code)).start();
+        if (parkAndCheckInterrupt()){
+            throw new InterruptedException();
+        }
+        return resource.getResource();
+    }
+
+    private final boolean parkAndCheckInterrupt() {
+        LockSupport.park(this);
+        return Thread.interrupted();
+    }
+}
