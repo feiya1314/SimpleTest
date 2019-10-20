955454a8a9ed9f43416ab9db77679f61918d997d
feiya1314 <yufei1313@gmail.com>
2019-10-20 16:14:48 +0800

    添加netty test

---
+++ src/main/java/mytest/netty/server/NettyServerTest.java
@@ -0,0 +1,10 @@
+package mytest.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+public class NettyServerTest {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        //serverBootstrap.
+    }
+}
