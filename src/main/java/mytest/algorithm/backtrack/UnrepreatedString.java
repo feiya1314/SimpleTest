5bfda3715f001b728aa60307166a6984315363ce
feiya1314 <yufei1313@gmail.com>
2020-12-24 21:41:12 +0800

    添加leetcode来源

--- src/main/java/mytest/algorithm/backtrack/UnrepreatedString.java
+++ src/main/java/mytest/algorithm/backtrack/UnrepreatedString.java
@@ -5,6 +5,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * 无重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合，字符串每个字符均不相同。
+ *
+ * 示例1:
+ *
+ *  输入：S = "qwe"
+ *  输出：["qwe", "qew", "wqe", "weq", "ewq", "eqw"]
+ * 示例2:
+ *
+ *  输入：S = "ab"
+ *  输出：["ab", "ba"]
+ * 提示:
+ *
+ * 字符都是英文字母。
+ * 字符串长度在[1, 9]之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutation-i-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author : yufei
  * @date : 2020/12/21 19:46
  * @description :
