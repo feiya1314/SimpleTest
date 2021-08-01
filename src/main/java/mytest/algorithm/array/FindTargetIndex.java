28ea795a560ba55721dbe2094f8f9cc55f17b820
feiya1314 <yufei1313@gmail.com>
2021-08-01 11:39:58 +0800

    二分查找

--- src/main/java/mytest/algorithm/array/FindTargetIndex.java
+++ src/main/java/mytest/algorithm/array/FindTargetIndex.java
@@ -66,4 +66,31 @@ public class FindTargetIndex {
         }
         return -1;
     }
+
+    private static int getIndex2(int[] source, int target) {
+        if (source == null || source.length < 1) {
+            return -1;
+        }
+        Arrays.sort(source);
+        int right = source.length - 1;
+        int left = 0;
+        while (left <= right) {
+            // 可以一定程度上防溢出
+            // pivot = (right + left) / 2，为啥就是中间值索引？有些人好好思考过吗？其实，这不过是一个运算的结果，真正原始的等式就是 pivot = left + (right - left) / 2;
+            // 这个等式才能从逻辑上说明中间值的获取过程，只是这个公式也刚好防范了溢出问题。
+            int mid = left + (right - left) / 2;
+            if (source[mid] == target) {
+                return mid;
+            }
+
+            if (source[mid] > target) {
+                right = mid - 1;
+                continue;
+            }
+            if (source[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
