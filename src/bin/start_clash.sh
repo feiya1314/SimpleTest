6a8385c9c4aae60694b28819f54ddb01e2c75090
feiya <yufei1313@gmail.com>
2022-12-24 13:27:11 +0800

    uopdate clash sh

--- src/bin/start_clash.sh
+++ src/bin/start_clash.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
-pid=`ps -ef | grep clash | grep config | awk '{print $2}'`
+pid=`ps -ef | grep clash | grep config.yml | awk '{print $2}'`
+
+clash_home=~/apps/clash
 
 if [ "x$pid" != "x" ]; then
   echo "clash running,pid : $pid"
-	notify-send  -i /home/feiya/clash/clashon.png "clash 代理" "clash 正在运行中"
+	notify-send  -i $clash_home/clashon.png "clash 代理" "clash 正在运行中"
   exit 0
 fi
 
-notify-send  -i /home/feiya/clash/clashon.png "clash 代理" "clash 开启"
-cd /home/feiya/clash
+notify-send  -i $clash_home/clashon.png "clash 代理" "开启 clash"
+cd $clash_home
 
 nohup ./clash -f config.yml 2>&1 > ./clash.log &
 
--- src/bin/stop_clash.sh
+++ src/bin/stop_clash.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
-pid=`ps -ef | grep clash | grep config | awk '{print $2}'`
+pid=`ps -ef | grep clash | grep config.yml | awk '{print $2}'`
+
+clash_home=~/apps/clash
 
 if [ "x$pid" != "x" ]; then
   echo "kill clash ,pid : $pid"
-	kill $pid
-	notify-send  -i /home/feiya/clash/clashoff.png "clash 代理" "clash 已关闭"
+    kill $pid
+    notify-send  -i $clash_home/clashoff.png "clash 代理" "clash 已关闭"
 else
-	notify-send  -i /home/feiya/clash/clashoff.png "clash 代理" "clash 未开启"
+    notify-send  -i $clash_home/clashoff.png "clash 代理" "clash 未开启"
 fi
