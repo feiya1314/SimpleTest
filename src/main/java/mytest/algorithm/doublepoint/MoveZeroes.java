7fce40a5d61d78ea9c2ea8d9c7a6529c866e0fe9
feiya1314 <yufei1313@gmail.com>
2026-04-23 20:35:26 +0800

    更新新做算法

--- src/main/java/mytest/algorithm/doublepoint/MoveZeroes.java
+++ src/main/java/mytest/algorithm/doublepoint/MoveZeroes.java
@@ -23,7 +23,7 @@ package mytest.algorithm.doublepoint;
 public class MoveZeroes {
     // 双指针，整体思想是把非 0 数移动到左边，左指针记录下一次可以移动的数字的位置，左指针前的数都是非0数，
     // 右指针遍历，最后把左指针及之后的位置补 0
-    public void moveZeroes(int[] nums) {
+    public void moveZeroes2(int[] nums) {
         if (nums == null || nums.length < 2) {
             return;
         }
@@ -40,6 +40,49 @@ public class MoveZeroes {
         while (left < nums.length) {
             nums[left] = 0;
             left++;
+        }
+    }
+
+    public void moveZeroes(int[] nums) {
+        int slowP = 0;
+        int fastP = 0;
+        // 一次遍历，如果当前值是0，则快指针往前走，如果不是0，则两个指针交换值，继续往下走
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                fastP++;
+                continue;
+            }
+            // 如果同一个位置，不用交换，继续往下
+            if (fastP == slowP) {
+                slowP++;
+                fastP++;
+                continue;
+            }
+            // 不是同一个位置，说明慢指针指向0的位置，交换下值，当前i位置置为 0，注意这种方式fastP == slowP必须要有
+            // 不然相同位置，但是不为0的情况，会替换成0
+            nums[slowP] = nums[i];
+            nums[i] = 0;
+            slowP++;
+            fastP++;
+        }
+    }
+
+    public void moveZeroes3(int[] nums) {
+        int slowP = 0;
+        int fastP = 0;
+        // 一次遍历，如果当前值是0，则快指针往前走，如果不是0，则两个指针交换值，继续往下走
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                fastP++;
+                continue;
+            }
+
+            // 不为0 直接交换，这里覆盖了两种情况，两个指针位置相同和位置不同，此时交换效果都是一样
+            int temp = nums[slowP];
+            nums[slowP] = nums[i];
+            nums[i] = temp;
+            slowP++;
+            fastP++;
         }
     }
 }
