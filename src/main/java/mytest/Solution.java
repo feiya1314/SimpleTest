1e0595709e7960082fcb328491470226319d6e96
feiya1314 <yufei1313@gmail.com>
2026-08-24 20:44:05 +0800

    update bakresume simp v1

--- src/main/java/mytest/Solution.java
+++ src/main/java/mytest/Solution.java
@@ -1,70 +1,55 @@
 package mytest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author : feiya
  * @date : 2021/3/20
  * @description :
  */
-public class Solution {
-    public static void main(String[] args) {
-        String result = new Solution().desc("tree");
-        System.out.println(result);
-    }
-
-    public String desc(String input) {
-        if (input == null || input.length() <= 1) {
-            return input;
-        }
-        char[] chars = input.toCharArray();
-        List<Character> cList = new ArrayList<>();
-        Map<Character, Integer> charsMap = new HashMap<>();
-        for (char c : chars) {
-            charsMap.merge(c, 1, Integer::sum);
-        }
-        Map<Integer, List<Character>> freq = new HashMap<>();
-        charsMap.forEach((k, v) -> {
-            List<Character> charsList = freq.get(v);
-            if (charsList == null) {
-                charsList = new ArrayList<>();
-                freq.put(v, charsList);
-            }
-            charsList.add(k);
-        });
-
-
-
-        int[] cFreq = new int[freq.keySet().size()];
-        int i = 0;
-        for (int f : freq.keySet()) {
-            cFreq[i] = f;
-            i++;
-        }
-        Arrays.sort(cFreq);
-
-    /*    Set<Character> cSet = new HashSet<>();
-        for (int i = 0; i < chars.length; i++) {
-            if (cSet.contains(chars[i])) {
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<String> stack = new ArrayDeque<>();
+        for (String token : tokens) {
+            if ("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token)) {
+                String result = doCalc(stack, token);
+                stack.push(result);
                 continue;
             }
-            cSet.add(chars[i]);
-            cList.add(chars[i]);
+            stack.push(token);
         }
-        StringBuilder sb = new StringBuilder();
-        for (Character c : cList) {
-            int num = charsMap.get(c);
-            for (int i = 0; i < num; i++) {
-                sb.append(c);
-            }
-        }*/
 
-        return null;
+        return Integer.parseInt(stack.poll());
+    }
+
+    private String doCalc(Deque<String> stack, String token) {
+        String right = stack.poll();
+        String left = stack.poll();
+        if ("+".equals(token)) {
+            long c1 = Long.parseLong(left);
+            long c2 = Long.parseLong(right);
+            long result = c1 + c2;
+            return String.valueOf(result);
+        }
+        if ("-".equals(token)) {
+            long c1 = Long.parseLong(left);
+            long c2 = Long.parseLong(right);
+            long result = c1 - c2;
+            return String.valueOf(result);
+        }
+        if ("*".equals(token)) {
+            long c1 = Long.parseLong(left);
+            long c2 = Long.parseLong(right);
+            long result = c1 * c2;
+            return String.valueOf(result);
+        }
+        if ("/".equals(token)) {
+            long c1 = Long.parseLong(left);
+            long c2 = Long.parseLong(right);
+            long result = c1 / c2;
+            return String.valueOf(result);
+        }
+        throw new IllegalArgumentException();
     }
 }
--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,27 +1,61 @@
-import java.util.*;
 
 public class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        if (n <= 0) {
-            return Collections.emptyList();
-        }
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> track = new ArrayList<>();
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int nums1Len = nums1.length;
+        int nums2Len = nums2.length;
+        int[][] result = new int[nums1Len + nums2Len][];
 
-        backtrack(n, k, 1, result, track);
-        return result;
-    }
+        int maxLen = 0;
 
-    private void backtrack(int n, int k, int start, List<List<Integer>> result, List<Integer> track) {
-        if (track.size() == k) {
-            result.add(new ArrayList<>(track));
-            return;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < nums1Len + nums2Len; i++) {
+            if (left >= nums1Len && right >= nums2Len) {
+                break;
+            }
+            int lP = left < nums1Len ? nums1[left][0] : 0;
+            int lV = left < nums1Len ? nums1[left][1] : 0;
+
+            int rP = right < nums2Len ? nums2[right][0] : 0;
+            int rV = right < nums2Len ? nums2[right][1] : 0;
+
+            if (lP == rP) {
+                result[i] = new int[]{lP, lV + rV};
+                maxLen++;
+                left++;
+                right++;
+                continue;
+            }
+            if (rP == 0) {
+                result[i] = nums1[left];
+                maxLen++;
+                left++;
+                continue;
+            }
+            if (lP == 0) {
+                result[i] = nums2[right];
+                maxLen++;
+                right++;
+                continue;
+            }
+            if (lP < rP) {
+                result[i] = nums1[left];
+                maxLen++;
+                left++;
+                continue;
+            }
+            if (lP > rP) {
+                result[i] = nums2[right];
+                maxLen++;
+                right++;
+            }
         }
 
-        for (int i = start; i <= n; i++) {
-            track.add(i);
-            backtrack(n, k, i + 1, result, track);
-            track.removeLast();
+        int[][] realResult = new int[maxLen][];
+        for (int i = 0; i < maxLen; i++) {
+            realResult[i] = result[i];
         }
+
+        return realResult;
     }
 }
